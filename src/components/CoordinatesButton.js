e9c70f6154f7f0450b56f64a298abfbd39d6017f
cassymarie <cassy2205@gmail.com>
2021-02-25 13:53:42 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,32 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+        onReceiveCoordinates: []
+    };
+  }
+
+  handleClick = (e) => {
+    // Update our state here...
+   // this.setState({hasBeenClicked: true},() => console.log(this.state.hasBeenClicked))
+//    debugger
+   this.setState({onReceiveCoordinates: [e.clientX, e.clientY]},() => console.log(this.state.onReceiveCoordinates))
+    
+    
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>CoordinatesButton</button>
+      </div>
+    );
+  }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,33 @@
 // Code DelayedButton Component Here
+
+import React from 'react';
+
+class DelayedButton extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+        onDelayedClick: '',
+        number: 0
+    };
+  }
+
+  handleClick = (e) => {
+    // Update our state here...
+    setTimeout(()=>{
+        this.props.onDelayedClick(e)
+    }, this.props.delay)
+
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>DelayedButton</button>
+      </div>
+    );
+  }
+}
+
+export default DelayedButton;
